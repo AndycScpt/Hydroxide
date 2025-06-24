3cda2c84b97b54086f66f810ad96551c4e37dd3e
AndycScpt <90601886+AndycScpt@users.noreply.github.com>
2025-06-24 22:31:44 +0800

    Update RemoteSpy.lua

--- ui/modules/RemoteSpy.lua
+++ ui/modules/RemoteSpy.lua
@@ -63,7 +63,8 @@ local icons = {
     RemoteEvent = "rbxassetid://4229806545",
     RemoteFunction = "rbxassetid://4229810474",
     BindableEvent = "rbxassetid://4229809371",
-    BindableFunction = "rbxassetid://4229807624"
+    BindableFunction = "rbxassetid://4229807624",
+    UnreliableRemoteEvent = "rbxassetid://116756718965593"
 }
 
 local constants = {
@@ -434,11 +435,11 @@ function Log.adjust(log)
     local logIcon = logInstance.Icon
 
     local callWidth = TextService:GetTextSize(logInstance.Calls.Text, 18, "SourceSans", constants.textWidth).X + 10
-    local iconPosition = callWidth - (((remoteClassName == "RemoteEvent" or remoteClassName == "BindableEvent") and 4) or 0)
+    local iconPosition = callWidth - (((remoteClassName == "RemoteEvent" or remoteClassName == "UnreliableRemoteEvent" or remoteClassName == "BindableEvent") and 4) or 0)
     local labelWidth = iconPosition + 21
 
     logInstance.Calls.Size = UDim2.new(0, callWidth, 1, 0)
-    logIcon.Position = UDim2.new(0, iconPosition, 0.5, (remoteClassName == "RemoteEvent" and -9) or -7)
+    logIcon.Position = UDim2.new(0, iconPosition, 0.5, ((remoteClassName == "RemoteEvent" or remoteClassName == "UnreliableRemoteEvent") and -9) or -7)
     logInstance.Label.Position = UDim2.new(0, labelWidth, 0, 0)
     logInstance.Label.Size = UDim2.new(1, -labelWidth, 1, 0)
 end
@@ -497,6 +498,30 @@ local function refreshLogs()
     end
 
     remoteList:Recalculate()
+end
+
+if not ListFlags:FindFirstChild("UnreliableRemoteEvent") then
+    local remoteEventFlag = ListFlags:FindFirstChild("RemoteEvent")
+    if remoteEventFlag then
+        local unreliableFlag = remoteEventFlag:Clone()
+        unreliableFlag.Name = "UnreliableRemoteEvent"
+        unreliableFlag.Parent = ListFlags
+        
+        local label = unreliableFlag:FindFirstChild("Label")
+        if label then
+            label.Text = "UnreliableRemoteEvent"
+        end
+        
+        local icon = unreliableFlag:FindFirstChild("Icon")
+        if icon then
+            icon.Image = icons.UnreliableRemoteEvent
+        end
+        
+        local toggle = unreliableFlag:FindFirstChild("Toggle")
+        if toggle and toggle:FindFirstChild("Label") then
+            toggle.Label.Text = ""
+        end
+    end
 end
 
 for _i,flag in pairs(ListFlags:GetChildren()) do
@@ -837,7 +862,7 @@ scriptContext:SetCallback(function()
     local remotePath = getInstancePath(selectedRemote)
     local method
 
-    if remoteClassName == "RemoteEvent" then
+    if remoteClassName == "RemoteEvent" or remoteClassName == "UnreliableRemoteEvent" then
         method = "FireServer"
     elseif remoteClassName == "RemoteFunction" then
         method = "InvokeServer"
@@ -911,7 +936,7 @@ repeatCallContext:SetCallback(function()
     local remoteClassName = remoteInstance.ClassName
     local method 
 
-    if remoteClassName == "RemoteEvent" then
+    if remoteClassName == "RemoteEvent" or remoteClassName == "UnreliableRemoteEvent" then
         method = "FireServer"
     elseif remoteClassName == "RemoteFunction" then
         method = "InvokeServer"
